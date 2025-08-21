bddb4fb49e710f24943298f7d235d0df15c1f722
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-21 00:26:25 -0300

    Commit do projeto

---
+++ src/test/java/br/com/fiap/fiapcarros/FiapCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapcarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapCarrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
