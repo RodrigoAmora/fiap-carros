6a87c8661e270308bacaf362c07c647d1dca1291
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-09-05 02:34:12 -0300

    Adicionando a anotação TestPropertySource

--- src/test/java/br/com/fiap/fiapcarros/FiapCarrosApplicationTests.java
+++ src/test/java/br/com/fiap/fiapcarros/FiapCarrosApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.fiap.fiapcarros;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class FiapCarrosApplicationTests {
 
     @Test
